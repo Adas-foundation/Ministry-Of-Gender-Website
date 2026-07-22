794b2e3f20cf2b3607a5e5b43f03d458701c623b
bsc-com-44-24 <bsc-com-44-24@unima.ac.mw>
2026-07-22 10:22:07 +0200

    Updated app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { EvidenceModule } from './evidence/evidence.module';
+import { Evidence } from './evidence/entities/evidence.entity';
+
 
 @Module({
-  imports: [],
+  imports:[ 
+    ConfigModule.forRoot({ isGlobal: true }), 
+    TypeOrmModule.forRootAsync({ 
+      imports: [ConfigModule], 
+      inject: [ConfigService], 
+      useFactory: (config: ConfigService) => ({ 
+        type: 'postgres', 
+        host: config.get('DB_HOST'), 
+        port: parseInt(config.get('DB_PORT', '5432')), 
+        username: config.get('DB_USER'), 
+        password: config.get('DB_PASSWORD'), 
+        database: config.get('DB_NAME'), 
+        synchronize: config.get('DB_SYNCHRONIZE') == 'true', 
+        entities: [Evidence], 
+        logging: true, 
+         ssl: {
+          rejectUnauthorized: false,
+         },
+      }), 
+   }),
+
+   EvidenceModule,
+  ],
+   
   controllers: [AppController],
-  providers: [AppService],
+  providers : [AppService],
 })
+
 export class AppModule {}
