5a679f1ad48d1435fd5e4e313ad8b3aab80eb45c
wany4-dec <wanangwan639@gmail.com>
2026-07-29 15:34:13 +0200

    added Cors section in main.ts

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -1,8 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
 import { Role } from '../../roles/entities/role.entity';
 import { Report } from '../../reports/entities/report.entity';
-import { Evidence } from 'src/evidence/entities/evidence.entity';
-import { CaseStatusHistory } from 'src/case_status_history/entities/case_status_history.entity';
 
 @Entity('users')
 export class User {
@@ -23,13 +21,4 @@ export class User {
 
   @OneToMany(() => Report, (report) => report.assignedUser)
   assignedReports?: Report[];
-
-  @OneToMany(() => Evidence, evidence => evidence.uploadedBy)
-  uploadedEvidence!: Evidence[];
-
-  @OneToMany(
-  () => CaseStatusHistory,
-  statusHistory => statusHistory.changedBy
-)
-statusChanges!: CaseStatusHistory[];
 }
