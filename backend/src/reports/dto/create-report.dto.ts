4ffbeece43443ea07eabeeb50da4938ab6a6b1e9
wany4-dec <wanangwan639@gmail.com>
2026-08-11 09:05:04 +0200

    modified report dto

--- backend/src/reports/dto/create-report.dto.ts
+++ backend/src/reports/dto/create-report.dto.ts
@@ -1,14 +1,21 @@
-import { IsInt, IsNotEmpty, IsOptional, IsString, IsEnum  } from "class-validator";;
+import {IsEnum,IsInt,IsNotEmpty,IsOptional,IsString,} from 'class-validator';
+
 import { CaseStatus } from '../../case_status_history/enums/case_status_history.enum';
 
 export class CreateReportDto {
   @IsNotEmpty()
   @IsString()
   description?: string;
+
+  @IsOptional()
   @IsEnum(CaseStatus)
   status?: CaseStatus;
+
+  @IsNotEmpty()
   @IsInt()
   districtId?: number;
+
+  @IsOptional()
   @IsInt()
   assignedUserId?: number;
 }
